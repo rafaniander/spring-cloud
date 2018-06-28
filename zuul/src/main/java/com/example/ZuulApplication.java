9fd8e68b0a523782e2e3c1f2d06efd6fd9a0d5ff
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-28 20:14:26 -0300

    Mais alguns ajustes

--- zuul/src/main/java/com/example/ZuulApplication.java
+++ zuul/src/main/java/com/example/ZuulApplication.java
@@ -1,17 +1,13 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//@EnableDiscoveryClient
-//@SpringBootApplication
-//@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
-@SpringCloudApplication
+@SpringBootApplication
 public class ZuulApplication {
 
 	public static void main(String[] args) {
