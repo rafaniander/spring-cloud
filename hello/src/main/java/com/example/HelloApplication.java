7d85c660de83fffc9e7f87c8bf0d06c0c9a0287a
Rafael Marques <rafaelmarques1992@gmail.com>
2018-06-29 16:30:47 -0300

    Adicionando mais um cliente para o app Hello

--- hello/src/main/java/com/example/HelloApplication.java
+++ hello/src/main/java/com/example/HelloApplication.java
@@ -1,13 +1,9 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableDiscoveryClient
 @SpringBootApplication
 public class HelloApplication {
--- zuul/src/main/java/com/example/ZuulApplication.java
+++ zuul/src/main/java/com/example/ZuulApplication.java
@@ -1,14 +1,10 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
