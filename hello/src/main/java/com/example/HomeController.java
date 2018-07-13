e096b6f250ffd53c547a94c0558db79cb353cdd4
Rafael Marques <rafaelmarques1992@gmail.com>
2018-07-13 16:48:13 -0300

    Corrigindo a home

--- hello/src/main/java/com/example/HomeController.java
+++ hello/src/main/java/com/example/HomeController.java
@@ -5,13 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@EnableResourceServer
 @Controller
 @RefreshScope
 @EnableAutoConfiguration
